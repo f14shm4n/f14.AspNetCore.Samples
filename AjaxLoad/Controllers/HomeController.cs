138768c8b3b4ab4d8ce7426852580f914e08a30d
f14shm4n <uraborg@gmail.com>
2018-07-03 17:35:50 +0300

    Added descriptions. EN,RU

--- AjaxLoad/Controllers/HomeController.cs
+++ AjaxLoad/Controllers/HomeController.cs
@@ -23,11 +23,18 @@ namespace AjaxLoad.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            return View(_userRecordRepository.GetUserRecords().Take(ITEMS_PER_PAGE));
+            // Gets records starts from index = 0 and takes ITEMS_PER_PAGE items from source list.
+            var records = _userRecordRepository.GetUserRecords()
+                .Take(ITEMS_PER_PAGE);
+            return View(records); // Pass the records to the View; see /Views/Home/Index.cshtml
         }
 
+        // An improvised endpoint api for getting records. Example url: www.example.com/getUserRecords?offset=10
         [HttpGet("{offset}")]
-        public IActionResult GetUserRecords([FromQuery] int offset) => Json(_userRecordRepository.GetUserRecords().Skip(offset).Take(ITEMS_PER_PAGE));
+        public IActionResult GetUserRecords([FromQuery] int offset)
+            // Gets records starts from index = OFFSET and takes ITEMS_PER_PAGE items from source list.
+            // Then return data as JSON.
+            => Json(_userRecordRepository.GetUserRecords().Skip(offset).Take(ITEMS_PER_PAGE));
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
